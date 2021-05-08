b4a1d408614b977208be6fb78009984ab2611256
masaya-kawamura <lucky.number0128@gmail.com>
2021-05-08 02:49:41 +0000

    [Finish]air_conditioner_Final

--- air_conditioner/air_conditioner.rb
+++ air_conditioner/air_conditioner.rb
@@ -3,8 +3,12 @@ puts "室内温度を入力してください"
 input_lines << gets.to_i
 puts "設定温度を入力してください"
 input_lines << gets.to_i
-room = input_lines[0].to_i
-air_conditioner = input_lines[1].to_i
+puts "風量を入力してください"
+input_lines << gets.to_i
+
+room = input_lines[0]
+air_conditioner = input_lines[1]
+air_flow = input_lines[2]
 
 temperature_diff = (room - air_conditioner).abs
 
@@ -17,6 +21,18 @@ elsif temperature_diff > 4 && temperature_diff < 10 then
 elsif temperature_diff > 10 then
   require_time = 60
 end
+
+if air_flow == 1
+  require_time
+elsif air_flow == 2
+  require_time -= 5
+elsif air_flow == 3
+  require_time -= 10
+else
+  puts "風量の値を1〜3で入力してください"
+  exit
+end
+
 puts "--------------------------------------"
 puts "室内温度が設定温度になるまでの時間は「#{require_time}分」です。"
 puts "======================================"
